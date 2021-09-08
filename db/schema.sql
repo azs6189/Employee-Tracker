cbb6efdb0b2f049570409b5808c32346c64d792a
Allie Soliz <solizale@gmail.com>
2021-09-07 21:12:59 -0400

    updated schema.sql 'employee table'

--- db/schema.sql
+++ db/schema.sql
@@ -25,31 +25,13 @@ CREATE TABLE roles (
 );
 
 -- Creates the table "employee" within employees_db --
-CREATE TABLE roles (
+CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT,
-    title VARCHAR(30) NOT NULL,
-    movie_id INT,
-    review TEXT NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT NOT NULL,
     PRIMARY KEY(id),
-    FOREIGN KEY (movie_id)
-    REFERENCES movies(id)
-    ON DELETE SET NULL
-    PRIMARY KEY(id)
+    FOREIGN KEY (role_id) REFERENCES roles(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
-
-role
-
-id: INT PRIMARY KEY
-title: VARCHAR(30) to hold role title
-salary: DECIMAL to hold role salary
-department_id: INT to hold reference to department role belongs to
-
-employee
-
-id: INT PRIMARY KEY
-first_name: VARCHAR(30) to hold employee first nam
-last_name: VARCHAR(30) to hold employee last name
-role_id: INT to hold reference to employee role
-
-
-manager_id: INT to hold reference to another employee that is the manager of the current employee (null if the employee has no manager)
