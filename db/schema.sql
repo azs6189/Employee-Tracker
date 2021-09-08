3c5f8863ea2ff31b4bb7a6961681ae2979689fdb
Allie Soliz <solizale@gmail.com>
2021-09-07 21:18:08 -0400

    updated employee table syntax

--- db/schema.sql
+++ db/schema.sql
@@ -20,7 +20,7 @@ CREATE TABLE roles (
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INT,
-    FOREIGN KEY (department_id) REFERENCES departments(id)
+    FOREIGN KEY (department_id) REFERENCES departments(id),
     PRIMARY KEY(id)
 );
 
@@ -30,8 +30,8 @@ CREATE TABLE employee (
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT,
-    manager_id INT NOT NULL,
+    manager_id INT,
     PRIMARY KEY(id),
-    FOREIGN KEY (role_id) REFERENCES roles(id),
-    FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+    FOREIGN KEY (role_id) REFERENCES roles (id),
+    FOREIGN KEY (manager_id) REFERENCES employee (id) ON DELETE SET NULL
 );
