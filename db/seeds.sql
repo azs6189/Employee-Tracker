5ade5962b8c2823a1fb73a483da6fd1d13add45f
Allie Soliz <solizale@gmail.com>
2021-09-19 15:47:37 -0400

    seeds.sql: updated department and roles seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,2 +1,30 @@
 -- Use employees_db database --
 USE employees_db;
+
+-- Inserts new records in a table --
+
+-- Department Seeds --
+INSERT INTO departments (id, department_name)
+VALUES (1, "Controllers");
+
+INSERT INTO departments (id, department_name)
+VALUES (2, "Engineering");
+
+INSERT INTO departments (id, department_name)
+VALUES (3, "Legal");
+
+INSERT INTO departments (id, department_name)
+VALUES (4, "Operations");
+
+-- Roles Seeds --
+INSERT INTO roles (id, title, salary, department_id)
+VALUES (1, "Product Controller", 90000, 1);
+
+INSERT INTO roles (id, department_name)
+VALUES (2, "Software Engineer", 115000, 2);
+
+INSERT INTO roles (id, department_name)
+VALUES (3, "General Counsel", 180000, 3);
+
+INSERT INTO roles (id, department_name)
+VALUES (4, "Risk Ops", 85000, 4);
