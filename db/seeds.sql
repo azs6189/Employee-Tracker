9fc9f6ae33eeefae1d1eab001a7751e8500f2669
Allie Soliz <solizale@gmail.com>
2021-09-19 18:38:09 -0400

    seeds.sql: updated employees table seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,7 +3,7 @@ USE employees_db;
 
 -- Inserts new records in a table --
 
--- Department Seeds --
+-- Department table Seeds --
 INSERT INTO departments (id, department_name)
 VALUES (1, "Controllers");
 
@@ -16,7 +16,7 @@ VALUES (3, "Legal");
 INSERT INTO departments (id, department_name)
 VALUES (4, "Operations");
 
--- Roles Seeds --
+-- Roles table Seeds --
 INSERT INTO roles (id, title, salary, department_id)
 VALUES (1, "Product Controller", 90000, 1);
 
@@ -33,4 +33,23 @@ INSERT INTO roles (id, department_name)
 VALUES (5, "General Counsel", 180000, 3);
 
 INSERT INTO roles (id, department_name)
+VALUES (6, "Risk Ops", 85000, 4);
+
+-- Employee table Seeds --
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (1, "Product Controller", 90000, 1);
+
+INSERT INTO employee (id, title, salary, department_id)
+VALUES (2, "Legal Entity Controller", 95000, 1);
+
+INSERT INTO employee (id, department_name)
+VALUES (3, "Software Engineer", 115000, 2);
+
+INSERT INTO employee (id, department_name)
+VALUES (4, "Senior Software Engineer", 135000, 2);
+
+INSERT INTO employee (id, department_name)
+VALUES (5, "General Counsel", 180000, 3);
+
+INSERT INTO employee (id, department_name)
 VALUES (6, "Risk Ops", 85000, 4);
