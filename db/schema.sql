85903c81bc8fe9ca400fd65ba4c329721da1f1e0
Allie Soliz <solizale@gmail.com>
2021-09-20 01:21:32 -0400

    updated js file, package.json, and files added are gitignore, package-lock.json

--- db/schema.sql
+++ db/schema.sql
@@ -1,27 +1,27 @@
 -- Drops the employees_db --
-DROP DATABASE IF EXISTS employees_db;
+DROP DATABASE IF EXISTS employee_db;
 
 -- Creates the "employees_db" database if it exists currently --
-CREATE DATABASE employees_db;
+CREATE DATABASE employee_db;
 
 -- Use employees_db database --
-USE employees_db;
+USE employee_db;
 
 -- Creates the table "departments" within employees_db --
-CREATE TABLE departments (
-  id INT NOT NULL AUTO_INCREMENT,
-  department_name VARCHAR(30) NOT NULL,
-  PRIMARY KEY(id)
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
 );
 
 -- Creates the table "roles" within employees_db --
-CREATE TABLE roles (
+CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
-    department_id INT,
-    FOREIGN KEY (department_id) REFERENCES departments(id),
-    PRIMARY KEY(id)
+    department_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
 );
 
 -- Creates the table "employee" within employees_db --
@@ -29,9 +29,9 @@ CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT,
+    role_id INT NOT NULL,
     manager_id INT,
-    PRIMARY KEY(id),
-    FOREIGN KEY (role_id) REFERENCES roles (id),
-    FOREIGN KEY (manager_id) REFERENCES employee (id) ON DELETE SET NULL
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
+    FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE CASCADE
 );
