a047a575866959217316e0ed1e76b7d705837a4e
Allie Soliz <solizale@gmail.com>
2021-10-04 00:49:23 -0400

    updated files

--- Develop/db/seeds.sql
+++ Develop/db/seeds.sql
@@ -1,54 +1,22 @@
--- Use employees_db database --
-USE employee_trackerDB;
+INSERT INTO departments (department_name)
+VALUES 
+    ("Accounting"),
+    ("Finance"),
+    ("Advertising"),
+    ("Engineering")
+;
 
--- Inserts new records in a table --
+INSERT INTO roles (title, salary, department_id)
+VALUES 
+    ("Software Engineer II", "90000", 4),
+    ("Financial Analyst", "85000", 2),
+    ("Designer", "90000", 4),
+    ("Software Engineer I", "70000", 1)
+;
 
--- Department table Seeds --
-INSERT INTO department (name)
-VALUES ("Controllers");
-
-INSERT INTO department (name)
-VALUES ("Engineering");
-
-INSERT INTO department (name)
-VALUES ("Legal");
-
-INSERT INTO department (name)
-VALUES ("Operations");
-
--- Role table Seeds --
-INSERT INTO role (title, salary, department_id)
-VALUES ("Product Controller", 90000, 1);
-
-INSERT INTO role (title, salary, department_id)
-VALUES ("Legal Entity Controller", 95000, 1);
-
-INSERT INTO role (title, salary, department_id)
-VALUES ("Software Engineer", 115000, 2);
-
-INSERT INTO role (title, salary, department_id)
-VALUES ("Senior Software Engineer", 135000, 2);
-
-INSERT INTO role (title, salary, department_id)
-VALUES ("General Counsel", 180000, 3);
-
-INSERT INTO role (title, salary, department_id)
-VALUES ("Risk Ops", 85000, 4);
-
--- Employee table Seeds --
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Allie", "Soliz", 1, 1);
-
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("John", "Doe", 2, 2);
-
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Rachel", "Zhao", 3, 3);
-
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Susan", "Carter", 4, 4);
-
--- select all from tables
-SELECT * FROM department;
-SELECT * FROM role;
-SELECT * FROM employee;
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES 
+    ("Emma", "Baker", 1, NULL),
+    ("Merik", "Doe", 2, NULL),
+    ("Tommy", "John", 3, NULL),
+    ("Jennifer", "Bekka", 4, NULL)
---
+++ server.js
@@ -0,0 +1,171 @@
+const inquirer = require("inquirer");
+const questions = require("./util/inquire_questions.js");
+const cTable = require("console.table");
+const DB = require("./util/util");
+
+// Prompt user for action
+function init() {
+	inquirer.prompt(questions.options).then((data) => {
+		option(data);
+	});
+}
+
+// Display data or ask additional questions based on option selected
+function option(data) {
+	switch (data.options) {
+		// Print tables of departments, roles, and employees to console
+		case "View all departments":
+			DB.viewDepartments().then((res) => {
+				console.log("\n" + cTable.getTable(res));
+				init();
+			});
+			break;
+		case "View all roles":
+			DB.viewRoles().then((res) => {
+				console.log("\n" + cTable.getTable(res));
+				init();
+			});
+			break;
+		case "View all employees":
+			DB.viewEmployees().then((res) => {
+				console.log("\n" + cTable.getTable(res));
+				init();
+			});
+			break;
+		// Add a department
+		case "Add a department":
+			inquirer.prompt(questions.addDepartment).then((data) => {
+				addDepartment(data);
+			});
+			break;
+		// Add a role
+		case "Add a role":
+			inquirer.prompt(questions.addRole).then((data) => {
+				addRole(data);
+			});
+			break;
+		// Add an employee
+		case "Add an employee":
+			inquirer.prompt(questions.addEmployee).then((data) => {
+				addEmployee(data);
+			});
+			break;
+		// Update employee role
+		case "Update an employee role":
+			inquirer.prompt(questions.updateEmployeeRole).then((data) => {
+				console.log(data);
+				updateEmployee(data);
+			});
+			break;
+		// Delete department
+		case "Delete department":
+			inquirer.prompt(questions.deleteDepartment).then((data) => {
+				deleteDepartment(data);
+			});
+			break;
+		// Delete role
+		case "Delete role":
+			inquirer.prompt(questions.deleteRole).then((data) => {
+				deleteRole(data);
+			});
+			break;
+		// Delete employee
+		case "Delete employee":
+			inquirer.prompt(questions.deleteEmployee).then((data) => {
+				deleteEmployee(data);
+			});
+			break;
+		// Quit program
+		case "Quit":
+			break;
+	}
+}
+
+// Add a department
+function addDepartment(data) {
+	DB.addDepartment({ department_name: data.department_name }).then((res) => {
+		if (res.affectedRows === 1) {
+			console.log(`Successfully added ${data.department_name}`);
+		}
+		init();
+	});
+}
+
+// Delete a department
+function deleteDepartment(data) {
+	DB.deleteDepartment(data.department_name).then((res) => {
+		console.log(res);
+		if (res.affectedRows === 1) {
+			console.log(`Successfully deleted ${data.department_name}`);
+		}
+		init();
+	});
+}
+
+// Add a role
+function addRole(data) {
+	// Connect role to department by finding the department's id
+	DB.findId(data.department, "departments", "department_name").then((res) => {
+		DB.addRole({
+			title: data.newRole,
+			salary: data.salary,
+			department_id: res[0].id,
+		}).then((res) => {
+			if (res.affectedRows === 1) {
+				console.log(`Successfully added ${data.newRole} to ${data.department}`);
+			}
+			init();
+		});
+	});
+}
+
+// Delete a role
+function deleteRole(data) {
+	DB.deleteRole(data.title).then((res) => {
+		if (res.affectedRows === 1) {
+			console.log(`Successfully deleted ${data.title}`);
+		}
+		init();
+	});
+}
+
+// Add an employee
+function addEmployee(data) {
+	// Connect employee to role by finding role's id
+	DB.findId(data.role, "roles", "title").then((res) => {
+		DB.addEmployee({
+			first_name: data.firstName,
+			last_name: data.lastName,
+			role_id: res[0].id,
+		}).then((res) => {
+			if (res.affectedRows === 1) {
+				console.log(`Successfully added ${data.firstName} to ${data.role}`);
+			}
+			init();
+		});
+	});
+}
+
+// Update an employee
+function updateEmployee(data) {
+	DB.findId(data.role, "roles", "title").then((res) => {
+		DB.updateEmployee([res[0].id, data.employee]).then((res) => {
+			if (res.affectedRows === 1) {
+				console.log(`Successfully updated ${data.employee}`);
+			}
+			init();
+		});
+	});
+}
+
+// Delete an employee
+function deleteEmployee(data) {
+	DB.deleteEmployee(data.employeeName).then((res) => {
+		if (res.affectedRows === 1) {
+			console.log(`Successfully deleted ${data.employeeName}`);
+		}
+		init();
+	});
+}
+
+init();
